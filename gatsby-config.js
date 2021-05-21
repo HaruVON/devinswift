3471a153a34b640181e551ea2d9e723425c9e062
Devin Swift <dswift@devinswift.dev>
2021-05-20 18:59:44 -0700

    Switch to GatsbyJS

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteMetadata: {
+    title: "Devin Swift - Developer Portfolio",
+  },
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-offline",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
---
+++ src/components/nav.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function Nav(props) {
+    return <nav> This is a nav</nav>
+}
